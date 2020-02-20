62f456ae0835c4684d2441811eb36a5c3bf216d5
wulantoro <nugrahawulantoro@gmail.com>
2020-02-20 11:38:49 +0700

    design menu 20-02-20

--- app/src/main/java/com/example/karyawan/Activity/HomeActivity.java
+++ app/src/main/java/com/example/karyawan/Activity/HomeActivity.java
@@ -5,15 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.example.karyawan.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private ImageView ivuser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ivuser = findViewById(R.id.ivuser);
+
+        ivuser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ProfilActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
