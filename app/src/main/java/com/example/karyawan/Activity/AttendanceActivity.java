4da1f61496b56a861c93f29ad53851ca3cc3dec6
wulantoro <nugrahawulantoro@gmail.com>
2020-02-24 23:06:00 +0700

    tambah status 24-02-20

--- app/src/main/java/com/example/karyawan/Activity/AttendanceActivity.java
+++ app/src/main/java/com/example/karyawan/Activity/AttendanceActivity.java
@@ -5,23 +5,31 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.karyawan.Activity.Model.Absent;
 import com.example.karyawan.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class AttendanceActivity extends AppCompatActivity {
+
+    private TextView tvstatus;
 
     private RecyclerView recyclerView;
     private AbsentAdapter absentAdapter;
     private ArrayList<Absent> absentArrayList;
+    private Absent absent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendance);
+        tvstatus = findViewById(R.id.tvstatus);
 
+        List<Absent> results = new ArrayList<>();
+        results.add(absent);
         loadAbsent();
 
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
@@ -33,13 +41,21 @@ public class AttendanceActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
 
         recyclerView.setAdapter(absentAdapter);
+
+        if (absent.getJamMasuk().equalsIgnoreCase("08:01")) {
+            tvstatus.setText("Terlambat");
+        }
+
+
     }
 
     void loadAbsent() {
         absentArrayList = new ArrayList<>();
+
         absentArrayList.add(new Absent("Dimas Maulana", "07:45", "17:01", "12-02-2020"));
         absentArrayList.add(new Absent("Dimas Maulana", "07:30", "17:15", "12-02-2020"));
         absentArrayList.add(new Absent("Dimas Maulana", "07:33", "17:30", "12-02-2020"));
-        absentArrayList.add(new Absent("Dimas Maulana", "07:30", "17:14", "12-02-2020"));
+        absentArrayList.add(new Absent("Dimas Maulana", "08:01", "17:14", "12-02-2020"));
+
     }
 }
